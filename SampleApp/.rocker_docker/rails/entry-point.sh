fba2a239630d4b300d25cecb11095de31592d3db
indrajit <eendroroy@gmail.com>
2017-05-20 23:16:53 +0600

    remove docker configs

--- SampleApp/.rocker_docker/rails/entry-point.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "Waiting for database server to start properly ..." && sleep 30
-RAILS_ENV=development bundle exec rake db:create
-RAILS_ENV=development bundle exec rake db:migrate
-RAILS_ENV=development bundle exec rake db:seed
-RAILS_ENV=development bundle exec rails server -b 0.0.0.0 -p 5000
